461224a6512857930bcca24a5ae6efb5f8957310
ArkynaSource <jhoelhex6@gmail.com>
2025-04-07 21:56:02 +0700

    Second U.P.P 0.3

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,9 +8,9 @@ const About = () => {
             style={{ backgroundImage: `url(${bgImage})` }}>
             <div className="min-h-screen flex flex-col items-center justify-center text-[#F5F5F5] p-8">
                 <div className="max-w-3xl text-center">
-                    <h1 className="text-4xl font-bold mb-6">About Me</h1>
+                    <h1 className="text-4xl font-bold mb-6">About Me and this little project</h1>
                     <p className="text-lg">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar tempor euismod. Nunc scelerisque tempor libero, egestas tempus velit maximus non. Vestibulum sed tristique purus. Duis feugiat condimentum quam et commodo. Fusce vel pulvinar nunc. Integer aliquam urna ante, id consequat turpis aliquet vitae. Integer sed felis sed erat dapibus venenatis eu a erat. Integer vitae ante sit amet quam pretium blandit sed in tellus.
+                    I had a great time working on this project, even though I had very little time—an all-too-familiar habit of mine. I managed to finish it, albeit just barely. After numerous consultations, I finally submitted it on time. Most of the time was lost in developing the website's concept and theme, but overall, it was an enjoyable journey despite the learning curves, no need to read all of this honestly this is just a placeholder!
                     </p>
                 </div>
             </div>
